c9462683637c6b9dc31c478d46068c0541463038
laugonzalezo <gonzalezortizlau@gmail.com>
2023-03-14 16:05:36 -0500

    lesson schema

--- models/lesson.py
+++ models/lesson.py
@@ -1,13 +1,13 @@
-from sqlalchemy import Column, Integer, String, Boolean, Tuple
+from sqlalchemy import Column, Integer, String
 from config.database import Base
-
+from pydantic import UploadFile, File
 class Lesson (Base):
     __tablename__ = "lesson"
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
     description = Column(String)
-    resource = Column(Tuple[String])
+    resource = Column(UploadFile[String])
     
 
 
