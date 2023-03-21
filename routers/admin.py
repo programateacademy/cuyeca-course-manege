222d63d7b7b4fcd67af63f61ec118221f91d7661
carolinaviasus <carolinaviasus@outlook.com>
2023-03-21 14:46:26 -0500

    prueba

--- routers/admin.py
+++ routers/admin.py
@@ -80,7 +80,7 @@ async def update_admin(
 
 
 # this is a endpoint to prove react with fastapi
-@admin_router.get("/api", tags=['admins'])
-async def root():
-    return{"message":"Creación administradores"}
+# @admin_router.get("/api", tags=['admins'])
+# async def root():
+#     return{"message":"Creación administradores"}
 
